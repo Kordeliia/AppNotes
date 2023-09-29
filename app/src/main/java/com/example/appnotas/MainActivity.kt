b972e5a39f459d26ba8232e33f7b4f36798e48df
Kordeliia <lalvmun@hotmail.com>
2023-09-29 10:18:12 +0200

    commit visualizar listado

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.appnotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.appnotas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var noteAdapter : NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val data = mutableListOf(
+            Note(1, "Tarea 1"),
+            Note(2, "Tarea 2"))
+        noteAdapter = NoteAdapter(data, this)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = noteAdapter
+        }
+    }
+
+    override fun onLongClick(note: Note) {
     }
 }
